f441efe648ff7a81cc55aa004d8b09fa0ad1bb78
Francesco Aiello <aiellof@oregonstate.edu>
2021-04-15 10:28:54 -0700

    Pass line 2 test

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,10 @@
 class House
-  def line(_)
-    "This is the house that Jack built.\n"
+  def line(number)
+    case number
+    when 1
+      "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    end
   end
 end
