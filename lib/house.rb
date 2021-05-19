ce83b69d499c41d46fbad2fdf03b2db8830a8203
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-19 15:26:36 -0700

    Delete case 1 and 2

--- lib/house.rb
+++ lib/house.rb
@@ -13,10 +13,6 @@ class House
 
   def line(number)
     case number
-    when 1
-      "This is #{phrase(number)}the house that Jack built.\n"
-    when 2
-      "This is #{phrase(number)}the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
