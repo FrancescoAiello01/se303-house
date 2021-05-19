d22e0ecc2c2461454e113f3cb7145cc5144e3a5b
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-19 15:26:21 -0700

    Replace duplication with message send for case 1 and 2

--- lib/house.rb
+++ lib/house.rb
@@ -3,16 +3,20 @@ class House
     (1..12).collect { |line_number| line(line_number) }.join("\n")
   end
 
-  def phrase
-    "This is the malt that lay in"
+  def phrase(number)
+    if number == 2
+      "the malt that lay in "
+    else
+      ""
+    end
   end
 
   def line(number)
     case number
     when 1
-      "This is the house that Jack built.\n"
+      "This is #{phrase(number)}the house that Jack built.\n"
     when 2
-      "#{phrase} the house that Jack built.\n"
+      "This is #{phrase(number)}the house that Jack built.\n"
     when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     when 4
@@ -33,6 +37,8 @@ class House
       "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    else
+      "This is #{phrase(number)}the house that Jack built.\n"
     end
   end
 end
