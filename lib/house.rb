75d2d22feb31e39dc619f9acd09dd3fb94cfd052
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-27 19:57:36 -0700

    Create phrase array

--- lib/house.rb
+++ lib/house.rb
@@ -32,6 +32,22 @@ class House
     end
   end
 
+  def phrase_array
+    [
+      'the malt that lay in',
+      'the rat that ate',
+      'the cat that killed',
+      'the dog that worried',
+      'the cow with the crumpled horn that tossed',
+      'the maiden all forlorn that milked',
+      'the man all tattered and torn that kissed',
+      'the priest all shaven and shorn that married',
+      'the rooster that crowed in the morn that woke',
+      'the farmer sowing his corn that kept',
+      'the horse and the hound and the horn that belonged to'
+    ]
+  end
+
   def line(number)
     "This is #{phrase(number)}the house that Jack built.\n"
   end
