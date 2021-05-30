e5ca34fc0829810df6d7d83b811ea36fce04fb40
Francesco Aiello <aiellof@oregonstate.edu>
2021-05-29 18:34:38 -0700

    Implement array

--- lib/house.rb
+++ lib/house.rb
@@ -30,25 +30,27 @@ class House
     else
       ""
     end
+    phrase_array.last(number).join(" ")
   end
 
   def phrase_array
     [
-      'the malt that lay in',
-      'the rat that ate',
-      'the cat that killed',
-      'the dog that worried',
-      'the cow with the crumpled horn that tossed',
-      'the maiden all forlorn that milked',
-      'the man all tattered and torn that kissed',
-      'the priest all shaven and shorn that married',
-      'the rooster that crowed in the morn that woke',
-      'the farmer sowing his corn that kept',
-      'the horse and the hound and the horn that belonged to'
+    'the horse and the hound and the horn that belonged to',
+    'the farmer sowing his corn that kept',
+    'the rooster that crowed in the morn that woke',
+    'the priest all shaven and shorn that married',
+    'the man all tattered and torn that kissed',
+    'the maiden all forlorn that milked',
+    'the cow with the crumpled horn that tossed',
+    'the dog that worried',
+    'the cat that killed',
+    'the rat that ate',
+    'the malt that lay in',
+    'the house that Jack built'
     ]
   end
 
   def line(number)
-    "This is #{phrase(number)}the house that Jack built.\n"
+    "This is #{phrase(number)}.\n"
   end
 end
